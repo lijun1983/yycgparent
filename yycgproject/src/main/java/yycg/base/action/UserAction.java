76f9d6aeaf33e4e53f8fe51f63b1d37b79e7e154
15302243357@189.cn <li19830917>
2018-08-19 18:46:22 +0800

    用户管理 action控制 与 jsp 导入

--- yycgproject/src/main/java/yycg/base/action/UserAction.java
+++ yycgproject/src/main/java/yycg/base/action/UserAction.java
@@ -15,11 +15,24 @@ public class UserAction
 	@Autowired
 	private UserService userService;
 
-	//用户查询页面  /yycgproject/user
+
+	/**
+
+	 *@描述  TODO: 用户管理 将页面所需要的数据取出，传到页面
+
+	 *@参数  [model]
+
+	 *@返回值  java.lang.String
+
+	 *@创建人  lijun
+
+	 *@创建时间  2018/8/19 0019
+
+	 *@修改人和其它信息
+
+	 */
 	@RequestMapping("/queryuser")
 	public String queryuser(Model model)throws Exception{
-		//将页面所需要的数据取出，传到页面
-		//用户类型
 		return "/base/user/queryuser";
 	}
 
